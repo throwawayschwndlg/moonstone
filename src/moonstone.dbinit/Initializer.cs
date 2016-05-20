175132aae80457f430ed6c4a55121c4f19966686
schwindelig <david.szoeke@gmail.com>
2016-05-20 16:11:46 +0200

    create core project

---
+++ src/moonstone.dbinit/Initializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moonstone.dbinit
+{
+    public class DbInfo
+    {
+        public SqlConnection MyProperty { get; set; }
+    }
+
+    public class Initializer
+    {
+    }
+}
