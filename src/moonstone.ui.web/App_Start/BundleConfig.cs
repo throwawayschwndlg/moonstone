1740ea8eee81bdd1a3f5a6c92c7fcb178af9d996
schwindelig <david.szoeke@gmail.com>
2016-06-07 20:06:31 +0200

    added bower, implemented bundling

---
+++ src/moonstone.ui.web/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace moonstone.ui.web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = true;
+
+            // global
+            bundles.Add(new StyleBundle("~/bundles/global/css").Include(
+                "~/ semantic/dist/semantic.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/global/js").Include(
+                "~/semantic/dist/semantic.js"));
+        }
+    }
+}
